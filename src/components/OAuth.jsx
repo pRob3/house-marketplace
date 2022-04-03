59ec843fd77fd8caef9607f3b3d9982fc8e21041
pRob3 <bubblan@slaf.se>
2022-04-03 18:16:12 +0200

    missed async

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -9,28 +9,28 @@ function OAuth() {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const onGoogleClick = () => {
+  const onGoogleClick = async () => {
     try {
-        const auth = getAuth()
-        const provider = new GoogleAuthProvider()
-        const result = await signInWithPopup(auth, provider)
-        const user = result.user
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
 
-        // Check for user
-        const docRef = doc(db, 'users', user.uid)
-        const docSnap = await getDoc(docRef)
+      // Check for user
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
 
-        // If user dosen't exists, create it
-        if(!docSnap.exists()){
-            await setDoc(doc(db, 'users', user.uid), {
-                name: user.displayName,
-                email: user.email,
-                timestamp: serverTimestamp()
-            })
-        }
-        navigate('/')
+      // If user dosen't exists, create it
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate('/')
     } catch (error) {
-        toast.error('Could not authorize wqith Google')
+      toast.error('Could not authorize wqith Google')
     }
   }
 
